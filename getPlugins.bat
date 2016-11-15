d99599a80c22c7fd5ddd44cd4e54271cfa6ba674
Raphael Lehmann <raphael@rleh.de>
2016-11-15 01:47:38 +0100

    add scons plugin module

--- getPlugins.bat
+++ getPlugins.bat
@@ -1,2 +1,3 @@
 call git clone git://git.jetbrains.org/idea/android.git android
 call git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+call git clone git@github.com:rleh/intellij-plugin-scons.git scons
--- getPlugins.sh
+++ getPlugins.sh
@@ -1,2 +1,3 @@
 git clone git://git.jetbrains.org/idea/android.git android
 git clone git://git.jetbrains.org/idea/adt-tools-base.git android/tools-base
+git clone git@github.com:rleh/intellij-plugin-scons.git scons
